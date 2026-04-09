2bd755091d0b49dbc78d6da79dd98a4bb54738e0
Sergio Sacristan <sergiosacristan8@gmail.com>
2026-04-09 14:13:04 +0200

    feat: remove temporary WAL and SHM files from PocketBase data directory

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE_URL ?? 'http://127.0.0.1:8090');
+const pb = new PocketBase(import.meta.env.PUBLIC_POCKETBASE_URL);
 
 export default pb;
